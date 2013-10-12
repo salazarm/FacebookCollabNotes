90ac169415ef94fb2d7b065fccc5ba4c2ba33eb2
YoMarco Salazar <salazarm@mit.edu>
2013-10-11 21:40:51 -0700

    blah2

--- ourServer/server.js
+++ ourServer/server.js
@@ -1,32 +1,81 @@
 var io = require('socket.io').listen(9002);
 var Parse = require('node-parse-api').Parse;
 var Q = require('q');
+var _ = require('underscore')
 
 var APP_ID = "IKqazQRognbSIXaBXhOR3edkG23W24QtvweETH2g";
 var MASTER_KEY = "7IyaybDg1RDfvAMDeTQqE7HQx8CWpNJkiYinmZYc";
 var parseApp = new Parse(APP_ID, MASTER_KEY);
 
-var FBCollab ={
+
+
+
+var FBCollab = {
+
   parse: {
-    createNewNote: function(user) {
+
+    getUser: function(fbUser) {
       var response = Q.defer();
-      parseApp.insert("Note", {creator_id: user.id }, function(err, data) {
+
+      parseApp.findOne("Users", fbUser.id, function(err, data) {
+        if(err) {
+          response.resolve( false )
+        } else {
+          response.resolve( true )
+        }
+      });
+
+      return response.promise
+    },
+    
+    registerUser: function(fbUser) {
+      var response = Q.defer();
+
+      parseApp.insert("RegisteredUsers", {fb_id: fbUser.id }, function(err, data) {
+        if (err) {
+          response.reject( new Error("ParseAPI Error") )
+        } else {
+          response.resolve( true )
+        }
+      });
+
+      return Q.promise;
+    },
+
+    createNewNote: function(fbUser) {
+
+      var response = Q.defer();
+
+      parseApp.insert("Notes", {creator_id: user.id }, function(err, data) {
         if (err) {
           response.reject( new Error("ParseAPI Error") )
         } else {
           response.resolve( data )
         }
-      })
+      });
+
       return Q.promise;
     },
-    getFriends: function(user) {
+
+
+    getFriends: function(fbUser) {
       // Parse query here
-      parseApp.findMany("Users", {first: user.id  })
+      var response = Q.defer();
+
+      parseApp.findMany("Friendships", { first: user.id  }, function(err, data) {
+        if (err) {
+          response.reject(  new Error("ParseAPI Error")  );
+        } else {
+          // for( )
+        }
+      });
+
+      return Q.promise
     },
-    goOnline: function(user) {
+    goOnline: function(fbUser) {
       parseApp.update("Users", user.id, {online: true})
     }, 
-    goOffline: function(user) {
+    goOffline: function(fbUser) {
       parseApp.update("Users", user.id, {online: false })
     }
   }
@@ -39,14 +88,24 @@ sockets_store = {
 
 io.sockets.on('connection', function (socket) {
 
+
+  /*
+    If this person is not a "RegisteredUser" then register them.
+    Broadcast their information to everyone. Clients will handle whether or not they
+    care depending on whether or not they are friends with the person. Return friends
+  */
   socket.on("Online", function(data) {
+
+    /*
+      check if this person is a registered user
+    */
 
     sockets_store[id] = socket
     socket.set("User", data)
     io.sockets.emit("UserOnline", data )
-    
+
     friends = FBCOllab.parse.getFriends( user ) 
-    if ( ) {
+    if ( friends ) {
       // user exits
 
 
@@ -66,14 +125,18 @@ io.sockets.on('connection', function (socket) {
 
   socket.on('InviteFriend', function(data) {
 
+
   });
 
   socket.on("AcceptInvite", function(data) {
+    /*
+      Check if we have the 
+    */
 
   });
 
   socket.on("GetFriends", function(data) {
-
+    
   })
 
   socket.on('disconnect', function () {
@@ -84,4 +147,6 @@ io.sockets.on('connection', function (socket) {
   });
 
 });
+
+
 
